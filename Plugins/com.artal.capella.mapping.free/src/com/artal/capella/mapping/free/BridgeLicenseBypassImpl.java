dd2d95a4a2c378219ca470315fd8358f31f8b4bc
JLA <jonathan.lasalle@artal.fr>
2021-01-26 17:17:56 +0100

    Add sentinel protection and fix build
    Conflicts:
    	Plugins/com.artal.capella.mapping/src/com/artal/capella/mapping/ui/TransfoExportHandler.java

---
+++ Plugins/com.artal.capella.mapping.free/src/com/artal/capella/mapping/free/BridgeLicenseBypassImpl.java
@@ -0,0 +1,13 @@
+package com.artal.capella.mapping.free;
+
+import com.artal.capella.mapping.services.BridgeLicenseBypass;
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class BridgeLicenseBypassImpl implements BridgeLicenseBypass {
+
+	public String getPassword()
+	{
+		return "k:XvW_4H/weGFH_~";
+	}
+}
